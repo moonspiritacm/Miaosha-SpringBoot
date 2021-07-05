e4ecdba6a063e4d545152181f47d1620c15f326c
moonspiritacm <yefanbupt@gmail.com>
2021-07-05 16:08:44 +0800

    Add Mybatis Framework

--- src/main/java/com/moonspirit/springboot/miaosha/Application.java
+++ src/main/java/com/moonspirit/springboot/miaosha/Application.java
@@ -1,5 +1,9 @@
 package com.moonspirit.springboot.miaosha;
 
+import com.moonspirit.springboot.miaosha.dao.UserInfoDOMapper;
+import com.moonspirit.springboot.miaosha.dataobject.UserInfoDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Hello world!
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.moonspirit.springboot.miaosha"})
+@MapperScan("com.moonspirit.springboot.miaosha.dao")
 @RestController
 public class Application {
+
+    @Autowired
+    private UserInfoDOMapper userInfoDOMapper;
 
     @RequestMapping("/helloworld")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/user")
+    public String getUserName() {
+        UserInfoDO userInfoDO = userInfoDOMapper.selectByPrimaryKey(1);
+        if (userInfoDO == null) {
+            return "用户对象不存在！";
+        }
+        return userInfoDO.getName();
     }
 
     public static void main(String[] args) {
